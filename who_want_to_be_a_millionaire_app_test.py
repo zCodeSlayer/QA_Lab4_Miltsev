632f8e973d884eaf245e668999d9018c41cf2318
zCodeSlayer <milceffk@mail.ru>
2022-05-11 16:11:05 +0700

    Добавим ещё несколько тестов к функции up_bank

--- who_want_to_be_a_millionaire_app_test.py
+++ who_want_to_be_a_millionaire_app_test.py
@@ -5,3 +5,11 @@ class Test_Game_Process(unittest.TestCase):
     def test_bank(self):
         new_bank = up_bank(250)
         self.assertEqual(new_bank, 500)
+
+    def test_bank_500_to_1000(self):
+        new_bank = up_bank(500)
+        self.assertEqual(new_bank, 1000)
+
+    def test_bank_1000_to_2000(self):
+        new_bank = up_bank(1000)
+        self.assertEqual(new_bank, 2000)
